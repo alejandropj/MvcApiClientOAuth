114672f6e2ea009876e55c4bfb7073652e083517
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-11 11:17:59 +0200

    Add project files.

---
+++ Services/ServiceApiEmpleados.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Headers;
+
+namespace MvcApiClientOAuth.Services
+{
+    public class ServiceApiEmpleados
+    {
+        private string UrlApiEmpleados;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceApiEmpleados(IConfiguration configuration)
+        {
+            this.UrlApiEmpleados = 
+                configuration.GetValue<string>("ApiUrls:ApiEmpleados");
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+
+        }
+    }
+}
