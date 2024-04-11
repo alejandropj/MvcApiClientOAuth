ec67341c776944f6cf8da6e382946318f1594c18
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-11 13:18:39 +0200

    11/04

--- Controllers/ManagedController.cs
+++ Controllers/ManagedController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using MvcApiClientOAuth.Models;
 using MvcApiClientOAuth.Services;
+using System.Security.Claims;
 
 namespace MvcApiClientOAuth.Controllers
 {
@@ -23,13 +26,35 @@ namespace MvcApiClientOAuth.Controllers
             if(token == null)
             {
                 ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+                return View();
             }
             else
             {
                 ViewData["MENSAJE"] = "Ya tienes tu token";
                 HttpContext.Session.SetString("TOKEN", token);
+                ClaimsIdentity identity =
+                    new ClaimsIdentity
+                    (CookieAuthenticationDefaults.AuthenticationScheme,
+                    ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.Name, model.UserName));
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, model.Password));
+
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal, new AuthenticationProperties
+                {
+                    ExpiresUtc = DateTime.UtcNow.AddMinutes(30)
+                });
+                return RedirectToAction("Index", "Home");
+
             }
-            return View();
+            
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
